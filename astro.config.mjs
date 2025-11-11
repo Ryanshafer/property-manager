c3b43e87df137c96a6919ed45a179c832a4da09c
Ryan Shafer <Ryan.Shafer@gmail.com>
2025-11-11 19:32:36 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react()],
+  site: "https://ryanshafer.github.io/proto-local-guide/",
+  base: "/proto-local-guide/",
+  output: "static"
+});
